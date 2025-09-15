7a671e9d0e68911089cb3048c2736bf422dc15ec
James Stacy <jamesmichaelstacy@gmail.com>
2025-09-14 17:06:32 -0700

    added script for new workstation install

---
+++ .dotfile-scripts/new-workstation.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+git clone --separate-git-dir=$HOME/.dotfiles https://github.com/SirHexxus/.dotfiles.git tmpdotfiles
+rsync --recursive --verbose --exclude '.git' tmpdotfiles/ $HOME/
+rm -r tmpdotfiles
